05d949dc8a3f6435e7770255c32b386ef31751d0
Rodrigo Castanon de Carvalho <rodrigo.castanon@poli.ufrj.br>
2020-08-05 18:17:35 -0300

    redux-persist working

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import store from './src/redux/store';
+import {store, persistor} from './src/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
 import MyApp from './src/routes';
+import {ActivityIndicator} from 'react-native';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <MyApp/>
+      <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+        <MyApp/>
+      </PersistGate>
     </Provider>
     );
 }
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,8 +1,17 @@
 import {createStore} from 'redux';
 import {devToolsEnhancer} from 'redux-devtools-extension';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
 import reducers from '../reducers';
 
-const store = createStore(reducers, devToolsEnhancer());
+const persistConfig = {
+  key:'root',
+  storage:AsyncStorage
+}
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(persistedReducer, devToolsEnhancer());
+export const persistor = persistStore(store);
+
 
